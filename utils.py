7c941088aa0cc4c9f27863275626aecb5ff63b72
zhaozz-lab <18046506891@163.com>
2019-12-04 15:23:44 +0800

    the result

--- utils.py
+++ utils.py
@@ -1,121 +1,121 @@
-import sys
-import os
-import time
-import math
-import torch
-import numpy as np
-from PIL import Image, ImageDraw, ImageFont
-from torch.autograd import Variable
+# import sys
+# import os
+# import time
+# import math
+# import torch
+# import numpy as np
+# from PIL import Image, ImageDraw, ImageFont
+# from torch.autograd import Variable
 
-import struct # get_image_size
-import imghdr # get_image_size
+# import struct # get_image_size
+# import imghdr # get_image_size
+# # 
+# def sigmoid(x):
+#     return 1.0/(math.exp(-x)+1.)
 
-def sigmoid(x):
-    return 1.0/(math.exp(-x)+1.)
-
-def softmax(x):
-    x = torch.exp(x - torch.max(x))
-    x = x/x.sum()
-    return x
-
-
-def bbox_iou(box1, box2, x1y1x2y2=True):
-    if x1y1x2y2:
-        mx = min(box1[0], box2[0])
-        Mx = max(box1[2], box2[2])
-        my = min(box1[1], box2[1])
-        My = max(box1[3], box2[3])
-        w1 = box1[2] - box1[0]
-        h1 = box1[3] - box1[1]
-        w2 = box2[2] - box2[0]
-        h2 = box2[3] - box2[1]
-    else:
-        mx = min(box1[0]-box1[2]/2.0, box2[0]-box2[2]/2.0)
-        Mx = max(box1[0]+box1[2]/2.0, box2[0]+box2[2]/2.0)
-        my = min(box1[1]-box1[3]/2.0, box2[1]-box2[3]/2.0)
-        My = max(box1[1]+box1[3]/2.0, box2[1]+box2[3]/2.0)
-        w1 = box1[2]
-        h1 = box1[3]
-        w2 = box2[2]
-        h2 = box2[3]
-    uw = Mx - mx
-    uh = My - my
-    cw = w1 + w2 - uw
-    ch = h1 + h2 - uh
-    carea = 0
-    if cw <= 0 or ch <= 0:
-        return 0.0
-
-    area1 = w1 * h1
-    area2 = w2 * h2
-    carea = cw * ch
-    uarea = area1 + area2 - carea
-    return carea/uarea
-
-def bbox_ious(boxes1, boxes2, x1y1x2y2=True):
-    if x1y1x2y2:
-        mx = torch.min(boxes1[0], boxes2[0])
-        Mx = torch.max(boxes1[2], boxes2[2])
-        my = torch.min(boxes1[1], boxes2[1])
-        My = torch.max(boxes1[3], boxes2[3])
-        w1 = boxes1[2] - boxes1[0]
-        h1 = boxes1[3] - boxes1[1]
-        w2 = boxes2[2] - boxes2[0]
-        h2 = boxes2[3] - boxes2[1]
-    else:
-        mx = torch.min(boxes1[0]-boxes1[2]/2.0, boxes2[0]-boxes2[2]/2.0)
-        Mx = torch.max(boxes1[0]+boxes1[2]/2.0, boxes2[0]+boxes2[2]/2.0)
-        my = torch.min(boxes1[1]-boxes1[3]/2.0, boxes2[1]-boxes2[3]/2.0)
-        My = torch.max(boxes1[1]+boxes1[3]/2.0, boxes2[1]+boxes2[3]/2.0)
-        w1 = boxes1[2]
-        h1 = boxes1[3]
-        w2 = boxes2[2]
-        h2 = boxes2[3]
-    uw = Mx - mx
-    uh = My - my
-    cw = w1 + w2 - uw
-    ch = h1 + h2 - uh
-    mask = ((cw <= 0) + (ch <= 0) > 0)
-    area1 = w1 * h1
-    area2 = w2 * h2
-    carea = cw * ch
-    carea[mask] = 0
-    uarea = area1 + area2 - carea
-    return carea/uarea
-
-def nms(boxes, nms_thresh):
-    if len(boxes) == 0:
-        return boxes
-
-    det_confs = torch.zeros(len(boxes))
-    for i in range(len(boxes)):
-        det_confs[i] = 1-boxes[i][4]                
-
-    _,sortIds = torch.sort(det_confs)
-    out_boxes = []
-    for i in range(len(boxes)):
-        box_i = boxes[sortIds[i]]
-        if box_i[4] > 0:
-            out_boxes.append(box_i)
-            for j in range(i+1, len(boxes)):
-                box_j = boxes[sortIds[j]]
-                if bbox_iou(box_i, box_j, x1y1x2y2=False) > nms_thresh:
-                    #print(box_i, box_j, bbox_iou(box_i, box_j, x1y1x2y2=False))
-                    box_j[4] = 0
-    return out_boxes
+# def softmax(x):
+#     x = torch.exp(x - torch.max(x))
+#     x = x/x.sum()
+#     return x
 
 
+# def bbox_iou(box1, box2, x1y1x2y2=True):
+#     if x1y1x2y2:
+#         mx = min(box1[0], box2[0])
+#         Mx = max(box1[2], box2[2])
+#         my = min(box1[1], box2[1])
+#         My = max(box1[3], box2[3])
+#         w1 = box1[2] - box1[0]
+#         h1 = box1[3] - box1[1]
+#         w2 = box2[2] - box2[0]
+#         h2 = box2[3] - box2[1]
+#     else:
+#         mx = min(box1[0]-box1[2]/2.0, box2[0]-box2[2]/2.0)
+#         Mx = max(box1[0]+box1[2]/2.0, box2[0]+box2[2]/2.0)
+#         my = min(box1[1]-box1[3]/2.0, box2[1]-box2[3]/2.0)
+#         My = max(box1[1]+box1[3]/2.0, box2[1]+box2[3]/2.0)
+#         w1 = box1[2]
+#         h1 = box1[3]
+#         w2 = box2[2]
+#         h2 = box2[3]
+#     uw = Mx - mx
+#     uh = My - my
+#     cw = w1 + w2 - uw
+#     ch = h1 + h2 - uh
+#     carea = 0
+#     if cw <= 0 or ch <= 0:
+#         return 0.0
+
+#     area1 = w1 * h1
+#     area2 = w2 * h2
+#     carea = cw * ch
+#     uarea = area1 + area2 - carea
+#     return carea/uarea
+
+# def bbox_ious(boxes1, boxes2, x1y1x2y2=True):
+#     if x1y1x2y2:
+#         mx = torch.min(boxes1[0], boxes2[0])
+#         Mx = torch.max(boxes1[2], boxes2[2])
+#         my = torch.min(boxes1[1], boxes2[1])
+#         My = torch.max(boxes1[3], boxes2[3])
+#         w1 = boxes1[2] - boxes1[0]
+#         h1 = boxes1[3] - boxes1[1]
+#         w2 = boxes2[2] - boxes2[0]
+#         h2 = boxes2[3] - boxes2[1]
+#     else:
+#         mx = torch.min(boxes1[0]-boxes1[2]/2.0, boxes2[0]-boxes2[2]/2.0)
+#         Mx = torch.max(boxes1[0]+boxes1[2]/2.0, boxes2[0]+boxes2[2]/2.0)
+#         my = torch.min(boxes1[1]-boxes1[3]/2.0, boxes2[1]-boxes2[3]/2.0)
+#         My = torch.max(boxes1[1]+boxes1[3]/2.0, boxes2[1]+boxes2[3]/2.0)
+#         w1 = boxes1[2]
+#         h1 = boxes1[3]
+#         w2 = boxes2[2]
+#         h2 = boxes2[3]
+#     uw = Mx - mx
+#     uh = My - my
+#     cw = w1 + w2 - uw
+#     ch = h1 + h2 - uh
+#     mask = ((cw <= 0) + (ch <= 0) > 0)
+#     area1 = w1 * h1
+#     area2 = w2 * h2
+#     carea = cw * ch
+#     carea[mask] = 0
+#     uarea = area1 + area2 - carea
+#     return carea/uarea
+
+# def nms(boxes, nms_thresh):
+#     if len(boxes) == 0:
+#         return boxes
+
+#     det_confs = torch.zeros(len(boxes))
+#     for i in range(len(boxes)):
+#         det_confs[i] = 1-boxes[i][4]                
+
+#     _,sortIds = torch.sort(det_confs)
+#     out_boxes = []
+#     for i in range(len(boxes)):
+#         box_i = boxes[sortIds[i]]
+#         if box_i[4] > 0:
+#             out_boxes.append(box_i)
+#             for j in range(i+1, len(boxes)):
+#                 box_j = boxes[sortIds[j]]
+#                 if bbox_iou(box_i, box_j, x1y1x2y2=False) > nms_thresh:
+#                     #print(box_i, box_j, bbox_iou(box_i, box_j, x1y1x2y2=False))
+#                     box_j[4] = 0
+#     return out_boxes
 
 
 
-def read_truths(lab_path):
-    if not os.path.exists(lab_path):
-        return np.array([])
-    if os.path.getsize(lab_path):
-        truths = np.loadtxt(lab_path)
 
-        truths = truths.reshape(int(truths.size/5), 5) # to avoid single truth problem
+
+# def read_truths(lab_path):
+#     if not os.path.exists(lab_path):
+#         return np.array([])
+#     if os.path.getsize(lab_path):
+#         truths = np.loadtxt(lab_path)
+
+#         truths = truths.reshape(int(truths.size/5), 5) # to avoid single truth problem
         
-        return truths
-    else:
-        return np.array([])
+#         return truths
+#     else:
+#         return np.array([])
